86e31a923f1a30d9d555b9782319e7d9009baa52
Frank Calise <fcalise@gmail.com>
2022-10-04 16:03:52 -0400

    feat(e2e): Expo Go + detox

--- detox/firstTest.spec.js
+++ detox/firstTest.spec.js
@@ -1,19 +1,32 @@
 // For more info on how to write Detox tests, see the official docs:
 // https://github.com/wix/Detox/blob/master/docs/README.md
-
-const { reloadApp } = require("./reload")
+const { reloadApp } = require("detox-expo-helpers")
 
 describe("Example", () => {
-  beforeEach(async () => {
-    await reloadApp()
+  beforeAll(async () => {
+    await reloadApp({
+      // Optionally, you can also set some parameters when loading the app
+      // see https://github.com/wix/Detox/blob/master/docs/APIRef.DeviceObjectAPI.md
+      args: {
+        disableTouchIndicators: true,
+        delete: true,
+        languageAndLocale: {
+          language: "en-US",
+          locale: "en-US",
+        },
+        url: "exp://127.0.0.1:19000",
+      },
+      launchArgs: {
+        // see https://github.com/wix/Detox/blob/master/docs/APIRef.LaunchArgs.md
+        EXKernelDisableNuxDefaultsKey: true,
+      },
+    })
+    // ⚠️ Important! Wait for first element to ensure that app is booted
+    const welcomeHeading = element(by.id("welcome-heading"))
+    await waitFor(welcomeHeading).toBeVisible().withTimeout(10000)
   })
 
-  it("should have the login screen first", async () => {
-    await expect(element(by.id("login-heading"))).toBeVisible()
-  })
-
-  it("should go to welcome screen after logging in", async () => {
-    await element(by.id("login-button")).tap()
+  it("should have the welcome screen first", async () => {
     await expect(element(by.id("welcome-heading"))).toBeVisible()
   })
 })
